69a606bfef09935543ccbfb21be931946de97f0f
MatheusFontenele <matheus.fontenele@icloud.com>
2022-06-01 23:13:39 -0300

    feat: ensure create purchase mutation in graphql

--- pages/enroll.tsx
+++ pages/enroll.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import { CalendarIcon } from "@heroicons/react/solid";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { GetStaticProps } from "next";
-import { withPublicApollo } from "../lib/withPublicApollo";
 import { getServerPageGetProducts, ssrGetProducts } from "../graphql/generated/pagePublic";
 import { GetProductsQuery, useCreatePurchaseMutation } from "../graphql/generated/graphql";
 import { withApollo } from "../lib/withApollo";
